2aa49cf93d8451c83a01110572f3a26713021386
asif03 <babu033045@gmail.com>
2024-09-15 15:22:05 +0600

    Update Code

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -30,14 +30,16 @@ export default function Welcome({ events }: PageProps<{ events: [] }>) {
                 <link rel="icon" type="image/x-icon" href={company.favicon} />
             </Head>
             <div className="relative m-0 box-border p-0 text-black">
-                <header className="flex h-screen flex-col bg-green-500">
-                    <nav className="sticky z-10 h-10 w-1/2 bg-purple-500">
-                        Navbar
-                    </nav>
-                    <div className="absolute left-0 top-0 z-30 h-80 w-2/3 bg-black"></div>
+                <header className="h-30 sticky left-0 top-0 z-10 w-1/2 bg-purple-500">
+                    <nav className="">Navbar</nav>
                 </header>
-                <main className="h-96 bg-slate-800">Main Content</main>
-                <footer className="h-24 w-full bg-[#0B3756]">Footer</footer>
+                <div className="absolute left-0 top-0 h-screen w-2/3 bg-black"></div>
+                <main className="static h-[1024px] bg-slate-800">
+                    Main Content
+                </main>
+                <footer className="relative h-24 w-full bg-[#0B3756]">
+                    Footer
+                </footer>
             </div>
         </>
     );
